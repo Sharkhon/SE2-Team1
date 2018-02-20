99d0c349af71c6cb8babee2623b1d267ed6d90cc
Corey Thornton <cthornt8@my.westga.edu>
2018-02-20 17:44:04 -0500

    Created Category class, added data members

---
+++ BudgetApp/src/budget/model/Category.java
@@ -0,0 +1,15 @@
+package budget.model;
+
+/**
+ * Defines Category Class
+ * 
+ * @author Software Engineering Group 1
+ * @version 1
+ *
+ */
+public class Category {
+	private String name;
+	private int allocatedAmount;
+	private int spentAmount;
+
+}
