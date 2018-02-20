39de3c2bc8f79012b4e1a46f82e9d5eb91c95b0a
Corey Thornton <cthornt8@my.westga.edu>
2018-02-20 18:28:05 -0500

    Added 3 parameter constructor for category, added getters for data members

--- BudgetApp/src/budget/model/Category.java
+++ BudgetApp/src/budget/model/Category.java
@@ -12,4 +12,69 @@ public class Category {
 	private int allocatedAmount;
 	private int spentAmount;
 
+	/**
+	 * Constructor for Category
+	 * 
+	 * @precondition name != null AND name != empty AND allocatedAmount >= 0 AND
+	 *               spentAmount >= 0
+	 * @postcondition none
+	 * 
+	 * @param name
+	 * 			name of the category
+	 * @param allocatedAmount
+	 * 				amount given to category 
+	 * @param spentAmount
+	 * 				amount spent out by category
+	 */
+	public Category(String name, int allocatedAmount, int spentAmount) {
+		if(name == null || name.equals("")) {
+			throw new IllegalArgumentException("Must provide valid name for Category");
+		}
+		if(allocatedAmount < 0) {
+			throw new IllegalArgumentException("allocated amount must be positive amount");
+		}
+		if(spentAmount < 0) {
+			throw new IllegalArgumentException("spent amount must be positive amount");
+		}
+		this.name = name;
+		this.allocatedAmount = allocatedAmount;
+		this.spentAmount = spentAmount;
+	}
+
+	/**
+	 * gets name of the category
+	 * 
+	 * @precondition none 
+	 * @postcondition none
+	 * 
+	 * @return name of category
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * gets allocated amount for category
+	 * 
+	 * @precondition none
+	 * @postcondition none
+	 * 
+	 * @return allocated amount
+	 */
+	public int getAllocatedAmount() {
+		return allocatedAmount;
+	}
+
+	/**
+	 * gets spent amount for category
+	 * 
+	 * @precondition none
+	 * @postcondition none
+	 * 
+	 * @return spent amount
+	 */
+	public int getSpentAmount() {
+		return spentAmount;
+	}
+
 }
