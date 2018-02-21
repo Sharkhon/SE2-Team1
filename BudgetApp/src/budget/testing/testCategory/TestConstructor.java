9c005820c9b52f5f725845a377ae7309bf6a3939
Corey Thornton <cthornt8@my.westga.edu>
2018-02-21 10:00:58 -0500

    Testing and ran code coverage over Category class

--- BudgetApp/src/budget/testing/testCategory/TestConstructor.java
+++ BudgetApp/src/budget/testing/testCategory/TestConstructor.java
@@ -4,11 +4,56 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import budget.model.Category;
+
 class TestConstructor {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testNullName() {
+
+		IllegalArgumentException exception = assertThrows(IllegalArgumentException.class,
+				() -> new Category(null, 25, 2));
+		assertEquals("Must provide valid name for Category", exception.getMessage());
+
+	}
+
+	@Test
+	void testEmptyName() {
+
+		IllegalArgumentException exception = assertThrows(IllegalArgumentException.class,
+				() -> new Category("", 25, 2));
+		assertEquals("Must provide valid name for Category", exception.getMessage());
+
+	}
+
+	@Test
+	void testInvalidAllocatedAmount() {
+
+		IllegalArgumentException exception = assertThrows(IllegalArgumentException.class,
+				() -> new Category("Groceries", -500, 2));
+		assertEquals("allocated amount must be positive amount", exception.getMessage());
+
+	}
+
+	@Test
+	void testInvalidSpentAmount() {
+
+		IllegalArgumentException exception = assertThrows(IllegalArgumentException.class,
+				() -> new Category("Groceries", 500, -40));
+		assertEquals("spent amount must be positive amount", exception.getMessage());
+
+	}
+
+	@Test
+	void testValidConstructor() {
+		Category cat = new Category("Groceries", 500, 40);
+
+		assertEquals("Groceries", cat.getName());
+
+		assertEquals(500, cat.getAllocatedAmount());
+
+		assertEquals(40, cat.getSpentAmount());
+
 	}
 
 }
