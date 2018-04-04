e7f55d8755b8aac4e4cea7b0b5ca6119c4905c61
Corey Thornton <cthornt8@my.westga.edu>
2018-04-04 17:20:05 -0400

    Refactored out the private helpermethod, handlecreateCNewCatergory from New category in NewCatergoryViewController

--- BudgetApp/src/budget/codebehinds/NewCategoryViewController.java
+++ BudgetApp/src/budget/codebehinds/NewCategoryViewController.java
@@ -7,48 +7,54 @@ import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 
 public class NewCategoryViewController {
-	 	@FXML
-	    private Button CreateNewCategoryButton;
+	@FXML
+	private Button CreateNewCategoryButton;
 
-	    @FXML
-	    private TextField categoryNameBox;
+	@FXML
+	private TextField categoryNameBox;
 
-	    @FXML
-	    private Button CancelButton;
+	@FXML
+	private Button CancelButton;
 
-	    @FXML
-	    private TextField initalAllocatedAmount;
-	    private String nameText;
-	    
-	    private OverView overview;
+	@FXML
+	private TextField initalAllocatedAmount;
+	private String nameText;
 
-	    public NewCategoryViewController(OverView overview) {
-			this.overview = overview;
+	private OverView overview;
+
+	public NewCategoryViewController(OverView overview) {
+		this.overview = overview;
+	}
+
+	@FXML
+	public void initialize() {
+		this.initalAllocatedAmount.setText(this.nameText);
+	}
+
+	@FXML
+	public void CreateNewCategory(ActionEvent event) {
+		if (this.categoryNameBox.getText().isEmpty()) {
+			return;// TODO Show add title 
 		}
-	    
-	    @FXML
-	    public void initialize() {
-	    		this.initalAllocatedAmount.setText(this.nameText);
-	    }
-	    
-	    @FXML
-	    public void CreateNewCategory(ActionEvent event) {
-	    		if(this.categoryNameBox.getText().isEmpty()) {
-	    			return;//TODO Show add title
-	    		}
-	    	
-	    		if (this.initalAllocatedAmount.getText().isEmpty()) {
-	    			this.overview.addNewCategory(this.categoryNameBox.getText());
-	    		} else {
-	    			this.overview.addNewCategory(this.categoryNameBox.getText(), Double.parseDouble(this.initalAllocatedAmount.getText()));
-	    		}
-	    		
-	    		this.CancelButton.getScene().getWindow().hide();
-	    }
 
-	    @FXML
-	    public void Cancel(ActionEvent event) {
-	    		this.CancelButton.getScene().getWindow().hide();
-	    }
+		handleAddNewCategory();
 
+		this.CancelButton.getScene().getWindow().hide();
+	}
+
+	@FXML
+	public void Cancel(ActionEvent event) {
+		this.CancelButton.getScene().getWindow().hide();
+	}
+	
+	
+
+	private void handleAddNewCategory() {
+		if (this.initalAllocatedAmount.getText().isEmpty()) {
+			this.overview.addNewCategory(this.categoryNameBox.getText());
+		} else {
+			this.overview.addNewCategory(this.categoryNameBox.getText(),
+					Double.parseDouble(this.initalAllocatedAmount.getText()));
+		}
+	}
 }
