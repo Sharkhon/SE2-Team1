d27b4087531981f0fbe5cada787f05a2c62b1248
Javon Onuigbo <jonuigb1@my.westga.edu>
2018-04-16 18:05:19 -0400

    Changed some of the doubles to DoubleProperty
    created the test constructor for testBudget

---
+++ BudgetApp/src/budget/testing/testBudget/TestConstructor.java
@@ -0,0 +1,14 @@
+package budget.testing.testBudget;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestConstructor {
+
+	@Test
+	void testNameIsNull() {
+		
+	}
+
+}
